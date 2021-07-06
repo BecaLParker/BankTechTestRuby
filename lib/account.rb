7fa06eb4f213ca7e921a4f4fd8a65b65eeb98171
BecaLParker <becaparker@gmail.com>
2021-07-06 14:21:32 +0100

    Unit tests account statement

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,10 @@
 # Responsible for account attributes
 
 class Account
-  attr_reader :balance, :transaction_log
+  attr_reader :balance, :transaction_log, :account_id
 
   def initialize(balance = 0, transaction_log = [])
-    @account_id = "random generator needed?"
+    @account_id = rand(1000..9999)
     @balance = balance
     @transaction_log = transaction_log
   end
@@ -12,6 +12,10 @@ class Account
   def credit(transaction_date, amount)
     @balance += amount
     @transaction_log.push("#{transaction_date} || #{'%.2f' % amount} || || #{'%.2f' % @balance}")
+  end
+
+  def statement
+    @transaction_log
   end
 
  
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,10 +8,26 @@ describe Account do
     end
 
     it 'updates transaction log' do
-      p subject
       expect { subject.credit("01/01/2020", 10.00) }.to change {
                                                           subject.transaction_log
                                                         }.from([]).to(["01/01/2020 || 10.00 || || 10.00"])
+    end
+  end
+
+  describe '#statement' do
+    it 'returns the transaction log' do
+      expect(subject.statement).to eq([])
+    end
+
+    it 'returns one credit logged correctly' do
+      subject.credit("02/02/2020", 20.00)
+      expect(subject.statement).to eq(["02/02/2020 || 20.00 || || 20.00"])
+    end
+
+    it 'returns multiple credits logged correctly' do
+      subject.credit("02/02/2020", 20.00)
+      subject.credit("03/03/2020", 30.00)
+      expect(subject.statement).to eq(["02/02/2020 || 20.00 || || 20.00", "03/03/2020 || 30.00 || || 50.00"])
     end
   end
 end
