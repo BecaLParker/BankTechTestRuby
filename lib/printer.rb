ca054792ff81136c5643a62134cebda044519cb2
BecaLParker <becaparker@gmail.com>
2021-07-08 15:22:00 +0100

    Printer#print copes with one deposit to account

--- lib/printer.rb
+++ lib/printer.rb
@@ -3,13 +3,27 @@
 # Resposible for outputting statement in required format
 
 class Printer
-  def print(_transactions)
+  def print(transactions)
     puts header
+    puts body(transactions)
   end
 
   private
 
   def header
     'date || credit || debit || balance'
+  end
+
+  def body(transactions)
+    balance = 0
+
+    transactions.each do |transaction|
+      balance += transaction[1]
+      if (transaction[1]).positive?
+        return "#{transaction[0]} || #{'%.2f' % transaction[1]} || || #{'%.2f' % balance}"
+      else
+        return "#{transaction[0]} || || #{'%.2f' % transaction[1].abs} || #{'%.2f' % balance}"
+      end
+    end
   end
 end
