3ac8dddc79e4226e55edbfee1ae441547253565c
BecaLParker <becaparker@gmail.com>
2021-07-08 17:34:16 +0100

    Isolates bank unit tests from printer class using double

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -3,6 +3,9 @@
 require 'bank'
 
 describe Bank do
+  let(:printer_dummy) { double('printer', print: true) }
+  subject { Bank.new(printer_dummy) }
+
   it 'starts with no transactions' do
     expect(subject.transactions).to be_instance_of Array
     expect(subject.transactions.empty?).to be true
@@ -12,6 +15,7 @@ describe Bank do
     it 'throws error when amount is <= 0' do
       expect { subject.deposit('04/04/2000', 0) }.to raise_error('Transaction must be greater than 0')
     end
+
     it 'stores a credit transaction' do
       expect { subject.deposit('01/04/2020', 10.00) }.to change {
                                                            subject.transactions
@@ -32,16 +36,13 @@ describe Bank do
 
   describe '#print_statement' do
     it 'passes transaction history to printer' do
-      printer = instance_double('Printer')
+      expect(printer_dummy).to receive(:print).with([])
+      subject.print_statement
 
-      expect(printer).to receive(:print).with([])
+      subject.deposit('01/04/2020', 10.00)
 
-      bank = Bank.new(printer)
-      bank.print_statement
-
-      bank.deposit('01/04/2020', 10.00)
-      expect(printer).to receive(:print).with([['01/04/2020', 10.00]])
-      bank.print_statement
+      expect(printer_dummy).to receive(:print).with([['01/04/2020', 10.00]])
+      subject.print_statement
     end
   end
 end
