d30bdc54dcb88562a1ee8ff4051607618a5806a4
BecaLParker <becaparker@gmail.com>
2021-07-08 17:38:42 +0100

    Refactors decimal formatting

--- lib/printer.rb
+++ lib/printer.rb
@@ -19,10 +19,12 @@ class Printer
     lines = []
     transactions.each do |transaction|
       balance += transaction[1]
+      formatted_balance = format('%.2f', balance)
+      formatted_amount = format('%.2f', transaction[1].abs)
       lines << if (transaction[1]).positive?
-                 "#{transaction[0]} || #{'%.2f' % transaction[1]} || || #{'%.2f' % balance}"
+                 "#{transaction[0]} || #{formatted_amount} || || #{formatted_balance}"
                else
-                 "#{transaction[0]} || || #{'%.2f' % transaction[1].abs} || #{'%.2f' % balance}"
+                 "#{transaction[0]} || || #{formatted_amount} || #{formatted_balance}"
                end
     end
     lines.reverse
